c422a82e26f570203af5b4f75db4212467a91749
emirsahovic <emir.sahovic00@gmail.com>
2022-02-25 12:03:51 +0100

    Login functionality

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,43 @@
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
+const ErrorResponse = require('../utils/errorResponse');
 
 exports.registerUser = asyncHandler(async (req, res, next) => {
     const { name, email, password } = req.body;
 
     if (!name || !email || !password) {
-        return res.status(400).json({ success: false, message: 'Please enter your name, email and password' });
+        return next(new ErrorResponse('Please provide name, email and password', 400));
     }
 
-    const userMatch = await User.findOne({ email: req.body.email });
+    const userMatch = await User.findOne({ email });
 
     if (userMatch) {
-        return res.status(400).json({ success: false, message: 'User with that email already exists' });
+        return next(new ErrorResponse('User with that email already exists', 400));
     }
 
     const user = await User.create(req.body);
 
     res.status(201).json({ success: true, data: user });
+})
+
+exports.loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    res.status(200).json({ success: true });
 })
