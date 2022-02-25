20d01066897dcca5be14f143eacf7d6e81aaa65c
emirsahovic <emir.sahovic00@gmail.com>
2022-02-25 19:50:46 +0100

    Update and delete course if user is logged in

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
-const { getCourses, createCourse } = require('../controllers/courses');
+const { getCourses, createCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 const router = express.Router({ mergeParams: true });
 const { protect } = require('../middleware/auth');
 
@@ -9,5 +9,7 @@ router.route('/').get(advancedResults(Course, {
     path: 'bootcamp',
     select: 'name description'
 }), getCourses).post(protect, createCourse);
+
+router.route('/:id').put(protect, updateCourse).delete(protect, deleteCourse);
 
 module.exports = router;
