5a33c4898db54a73b318f924495ba4793ae7d6e1
emirsahovic <emir.sahovic00@gmail.com>
2022-02-26 11:04:35 +0100

    Rate limiting, param pollution protection and CORS

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const errorHandler = require('./middleware/error');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
@@ -24,6 +27,15 @@ app.get('/', (req, res) => {
 app.use(mongoSanitize());
 app.use(helmet());
 app.use(xss());
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+})
+
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 app.use('/api/users', users);
 app.use('/api/bootcamps', bootcamps);
