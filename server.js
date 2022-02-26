5c26f114ab10e6d4bd9809f5f51b3beddc50af8b
emirsahovic <emir.sahovic00@gmail.com>
2022-02-26 11:02:35 +0100

    XSS protection

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const xss = require('xss-clean');
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,7 @@ app.get('/', (req, res) => {
 
 app.use(mongoSanitize());
 app.use(helmet());
+app.use(xss());
 
 app.use('/api/users', users);
 app.use('/api/bootcamps', bootcamps);
